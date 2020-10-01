4a30a72e12671a1c5b803cd992455e2b6169a5ca
Rogerio Souza <rogerio_dsouza@outlook.com>
2020-09-30 23:41:08 -0300

    Create unary_operators.js

---
+++ src/Chapter_1/unary_operators.js
@@ -0,0 +1,12 @@
+/*
+OPERADORES UNÁRIOS
+
+Nem todos operadores são símbolos. Alguns são excritos como palavras.
+Um exemplo é o tipo de operador, que  produz um valor de string que nomeia
+o tipo do valor que nós fornecemos: 
+*/
+
+console.log(typeof 4.5)
+// number
+console.log(typeof "x")
+// string
