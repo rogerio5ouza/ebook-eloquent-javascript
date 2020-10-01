f0d679ae7f3fa3251814d2a5785eb7ea44648488
Rogerio Souza <rogerio_dsouza@outlook.com>
2020-10-01 19:59:50 -0300

    Update boolean_value.js

--- src/Chapter_1/boolean_value.js
+++ src/Chapter_1/boolean_value.js
@@ -10,3 +10,19 @@ console.log(3 > 2)
 // true
 console.log(3 < 2)
 // false
+
+// Strings podem ser comparadas da mesma maneira:
+
+console.log("Java" < "JavaScript")
+
+// Outros operadores semelhantes são >= (maior ou igual), <= (menor ou igual), == (igual a ) e != (diferente de):
+
+console.log("Java" != "Kotlin")
+// true
+
+console.log("Apple" == "Orange");
+// false
+
+// Existe apenas um valor em JavaScript que não é igual a ele mesmo, que é NaN ("Not a Number"):
+
+console.log(NaN == NaN);
