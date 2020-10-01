f8d447d7f5961b1cd0c51801327eb21a34c55ee8
Rogerio Souza <rogerio_dsouza@outlook.com>
2020-09-30 23:49:29 -0300

    Create unary_operators.js

---
+++ src/Chapter_1/unary_operators.js
@@ -0,0 +1,20 @@
+/*
+OPERADORES UNÁRIOS
+
+Nem todos operadores são símbolos. Alguns são excritos como palavras.
+Um exemplo é o tipo de operador, que  produz um valor de string que nomeia
+o tipo do valor que nós fornecemos: 
+*/
+
+console.log(typeof 4.5)
+// number
+console.log(typeof "x")
+// string
+
+/*
+Os operadores que usam dois valores são chamados de operadores binários, enquanto os que usam um
+são chamados de operadores unários. O operador menos (-) pode ser usado como operador binário e como  operador unário:
+*/
+
+console.log(-(10 - 2))
+// -8
