02003073090bad82d5e466588abd44a974e703f0
Rogerio Souza <rogerio_dsouza@outlook.com>
2020-10-05 10:47:37 -0300

    Create functions.js

---
+++ src/Chapter_2/functions.js
@@ -0,0 +1,19 @@
+/*
+prompt()
+
+A função prompt() não é muito usada na programação web moderna, pricipalmente porque não temos controle
+sobre sua aparência do diálogo resultante, mas pode ser útil em programas de exemplos e experimentos:
+*/
+
+prompt("Enter password");
+
+/*
+console.log()
+
+A maioria dos sistemas JavaScript(incluindo navegadores e Node.js) fornecem uma função console.log que grava
+seus argumentos em algum dispositivo de saída de texto. No navegador, a saída chega ao console JavaScript. Esta
+parte da interface do navegador fica oculta por padrão, mas a maioria dos navegadores a abre
+quando pressionamos F12.
+*/
+
+console.log();
