e075ba5760bd36a0c3b07503f2f09005d0e12ad0
Rogerio Souza <rogerio_dsouza@outlook.com>
2020-10-15 22:54:36 -0300

    Update for_loops.js

--- src/Chapter_2/for_loops.js
+++ src/Chapter_2/for_loops.js
@@ -1,5 +1,17 @@
-/*Programa que imprime números pares.*/
+/* Como esse padrão é tão comum, JavaScript e linguagens semelhantes 
+fornecem uma forma um pouco mais curta e abrangente, o loop For. */
+
+// Programa que imprime números pares.
 
 for (let number = 0; number <= 12; number = number + 2) {
     console.log(number);
 }
+
+// Este exemplo calcula 2 elevado a décima potência, usando For ao invés do While:
+
+let result = 1;
+for (let counter = 0; counter < 10; counter = counter + 1) {
+    result = result * 2;
+}
+
+console.log(result);
