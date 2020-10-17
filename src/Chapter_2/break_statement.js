368c1e3e63a1c29e9c3676ce381266dbd902c95f
Rogerio Souza <rogerio_dsouza@outlook.com>
2020-10-17 08:14:40 -0300

    Update break_statement.js

--- src/Chapter_2/break_statement.js
+++ src/Chapter_2/break_statement.js
@@ -7,3 +7,9 @@ for (let current = 20; ; current = current + 1) {
         break
     }
 }
+
+/*
+A instrução Continue é semelhante a Break, pois influencia o progresso de um loop.
+Quando Continue é encontrado em um corpo de loop, o controle salta para fora do corpo e continua
+com a próxima iteração do loop.
+*/
